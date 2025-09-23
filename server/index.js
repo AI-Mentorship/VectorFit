4e657ec4a6973af40b2e4687df8522d8bc2831ef
Traman2 <tejassraman@gmail.com>
2025-09-22 22:53:48 -0500

    init node server

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("MongoDB Connection failed", err));
+  
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server running on PORT ${PORT}...`);
+});
+
+export default app;
