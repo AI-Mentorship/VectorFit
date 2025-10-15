6fcf81763b98aca7ce4dee3143b6efc9772f2e51
Justin Truong <justintruong222@gmail.com>
2025-10-14 19:02:56 -0500

    renamed a file

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout() {
       }}
     >
       <Tabs.Screen
-        name="index"
+        name="home"
         options={{
           title: "Home",
           tabBarIcon: ({ color, size }) => (
