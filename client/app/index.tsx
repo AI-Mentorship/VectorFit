e3d982b2fce05a8fc297e475732b56529e045f46
Justin Truong <justintruong222@gmail.com>
2025-10-14 23:10:35 -0500

    Added placeholders in homepage

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Text, View, useWindowDimensions } from "react-native";
 import {
   useFonts,
   SawarabiGothic_400Regular,
@@ -6,6 +6,9 @@ import {
 
 export default function Index() {
   const [fontsLoaded] = useFonts({ SawarabiGothic_400Regular });
+  const deviceHeight = useWindowDimensions().height;
+  const middleBoxHeight = (deviceHeight * 6) / 9;
+  const lowerBoxHeight = (deviceHeight * 1) / 9;
   if (!fontsLoaded) {
     return (
       <View
@@ -30,17 +33,46 @@ export default function Index() {
         paddingHorizontal: 20,
       }}
     >
+      {/* Top Box Greeting */}
       <Text
         style={{
           fontSize: 24,
           fontWeight: "800",
           marginBottom: 12,
+          marginLeft: 12,
           fontFamily: "SawarabiGothic_400Regular",
         }}
       >
         Welcome, User
       </Text>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+
+      {/* Placeholder box: For displaying outfits */}
+      <View
+        style={{
+          height: middleBoxHeight,
+          borderRadius: 12,
+          backgroundColor: "#e6e6e6",
+          marginTop: 12,
+          padding: 12,
+          justifyContent: "center",
+        }}
+      >
+        <Text>Middle placeholder box</Text>
+      </View>
+
+      {/* Placeholder Box: For the AI chatbox */}
+      <View
+        style={{
+          height: lowerBoxHeight,
+          borderRadius: 12,
+          backgroundColor: "#e6e6e6",
+          marginTop: 12,
+          padding: 12,
+          justifyContent: "center",
+        }}>
+          <Text>Bottom placeholder box</Text>
+      </View>
+
     </View>
   );
 }
