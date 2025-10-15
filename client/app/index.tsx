7bc63dc80cd3fa7aeee2602821d42231f81b3373
Justin Truong <justintruong222@gmail.com>
2025-10-14 22:52:59 -0500

    Using Sawarbi Gothic font

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,16 +1,46 @@
 import { Text, View } from "react-native";
+import {
+  useFonts,
+  SawarabiGothic_400Regular,
+} from "@expo-google-fonts/sawarabi-gothic";
 
 export default function Index() {
+  const [fontsLoaded] = useFonts({ SawarabiGothic_400Regular });
+  if (!fontsLoaded) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>Loading…</Text>
+      </View>
+    );
+  }
+
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        justifyContent: "flex-start",
+        // alignItems: "center",
+        paddingTop: 60,
+        paddingHorizontal: 20,
       }}
     >
+      <Text
+        style={{
+          fontSize: 24,
+          fontWeight: "800",
+          marginBottom: 12,
+          fontFamily: "SawarabiGothic_400Regular",
+        }}
+      >
+        Welcome, User
+      </Text>
       <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Text>Welcome, User</Text>
     </View>
   );
 }
